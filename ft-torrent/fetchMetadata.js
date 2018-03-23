bb8184ee44c899c24e5add46f43cb5ca1f526fa4
Brice Waegeneire <brice.wge@gmail.com>
2018-03-23 02:51:39 +0100

    verify if metadata are correct

--- ft-torrent/fetchMetadata.js
+++ ft-torrent/fetchMetadata.js
@@ -7,7 +7,7 @@ const utils = require('./utils')
 const config = require('./config')
 
 var peers = []
-
+var metadata = null
 function fetchMetadata () {
   discover.on('peer', (peer, source) => {
     console.log('Peer (discovery):', peer, source)
@@ -21,23 +21,35 @@ function connectToPeers () {
   // let peer = peers[i]
   // if (peer.port === config.port) peer = peers[++i]
   // console.log(peer)
-  for (let i = 0; i < 5; i++) {
-    let peer = peers[13 + i]
+  for (let i in peers) {
+    let peer = peers[i]
     console.log(peer)
     const socket = new net.Socket()
+    const wire = new Protocol()
+    socket.pipe(wire).pipe(socket)
     socket.connect(peer.port, peer.address, () => {
-      const wire = new Protocol()
-      wire.use(ut_metadata())
-      socket.pipe(wire).pipe(socket)
+      // const wire = new Protocol()
+      wire.use(ut_metadata(metadata))
+      // socket.pipe(wire).pipe(socket)
       wire.handshake(config.infoHash, config.selfId, { dht: true })
       wire.on('handshake', (ih, ip, ext) => {
         wire.ut_metadata.fetch()
         console.log('received handshake:', ih, ip, ext)
+        // console.log(wire)
       })
       return wire.ut_metadata.on('metadata', function (rawMetadata) {
-        let metadata = bencode.decode(rawMetadata, 'utf8')
-        console.log(metadata)
-        return metadata
+        // try {
+        let info = bencode.decode(rawMetadata).info
+        let metadata = bencode.encode(info)
+        let computedHash = utils.sha1sync(metadata)
+        if (config.infoHash !== computedHash) {
+          console.log('FALSE!!!')
+        }
+        console.log(computedHash, config.infoHash)
+        console.log(info)
+        // process.exit()
+        // } catch (err) { metadata = null }
+        // return metadata
       })
     })
     socket.on('error', (err) => {
--- ft-torrent/utils.js
+++ ft-torrent/utils.js
@@ -1,7 +1,15 @@
+const crypto = require('crypto')
+
 exports.parsePeer = function (peer) {
   let res = peer.split(':')
   return {
     address: res[0],
     port: parseInt(res[1])
   }
+}
+
+exports.sha1sync = function (buf) {
+  return crypto.createHash('sha1')
+    .update(buf)
+    .digest('hex')
 }
