a129ada52b5cb6a84aefea132ed4c316ab3027ff
Brice Waegeneire <brice.wge@gmail.com>
2018-03-29 01:25:44 +0200

    refactor stream on two middleware

--- video/test.js
+++ video/test.js
@@ -2,134 +2,141 @@ const torrentStream = require('torrent-stream')
 const ffmpeg = require('fluent-ffmpeg')
 const express = require('express')
 const path = require('path')
-// const http = require('http')
 
+const app = express()
 // const hash = '3F8F219568B8B229581DDDD7BC5A5E889E906A9B'
 const hash = '88594AAACBDE40EF3E2510C47374EC0AA396C08E'
 // const hash = '10EA57E365DB30BDEE0EE24C97D720DC8E0AAD5B' // MKV
+// ce9156eb497762f8b7577b71c0647a4b0c3423e1 // MKV
 const nativeFormats = ['webm', 'mp4']
 const transcodeFormats = ['mkv', 'avi']
-// Stop the process with Ctrl+c
-// process.once('SIGINT', function () {
-//   process.kill(process.pid, 'SIGINT')
-// })
 
-// let server = http.createServer()
-let app = express()
 let torrentsDownloading = {}
 
 app.listen(3000, () => console.log('Hypertube is listening on port 3000!'))
 
-app.get('/stream/:hash', streamTorrent)
+// TODO Delete torrent at startup based on ctime
+
+// TODO Move this to app.js
+const torrentsPath = path.join('/goinfre/', process.env.USER)
+console.log(torrentsPath)
 
 // TODO Handle errors correctly
-function streamTorrent (req, res, next) {
+// Get a stream from the cache, a file or download it
+exports.get = function (req, res, next) {
   try {
-    if (false) { // TODO Test if torrent is the db
-      // TODO Get the stream from a file
-    } else if (torrentsDownloading[req.params.hash]) {
+    const infoHash = req.params.hash.toLowerCase()
+    if (torrentsDownloading[infoHash]) {
       console.log('Here you go, it\'s already downloading')
-      stream(req, res, torrentsDownloading[req.params.hash])
+      res.file = torrentsDownloading[infoHash]
+      next()
+    } else if (false) { // TODO Test if torrent is the db
+      // fs.s
+      next()
     } else {
       console.log('We will initiate a download in a short while')
-      const engine = torrentStream(req.params.hash)
-      engine.once('ready', () => downloadVideo(req, res, engine.files))
-      engine.once('idle', () => keepSeeding(req.params.hash, engine))
+      const engine = torrentStream(infoHash, {tmp: torrentsPath})
+      engine.once('ready', () => selectFile(engine, res, next))
+      engine.once('idle', () => torrentFinished(engine))
     }
   } catch (err) { next(err) }
 }
 
-// TODO Do something when the subtitles are downloaded
-function downloadVideo (req, res, files) {
-  // console.log(files)
-  for (let i in files) {
-    let file = files[i]
-    file.type = path.extname(file.name).toLowerCase().substr(1)
-    console.log(file.name, file.type)
-    if (file.type === 'srt') {
-      file.select()
-      continue
+exports.send = function (req, res, next) {
+  const range = req.headers.range
+  let head
+  let start = 0
+  let end = res.file.length - 1
+
+  if (range) {
+    const parts = range.replace(/bytes=/, '').split('-')
+    start = parseInt(parts[0], 10)
+    end = parts[1] ? parseInt(parts[1], 10) : end
+    head = {
+      'Content-Range': `bytes ${start}-${end}/${res.file.length}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': (end - start) + 1,
+      'Content-Type': `video/${res.file.type}`
     }
-    if (nativeFormats.includes(file.type)) {
-      stream(req, res, file)
-      torrentsDownloading[req.params.hash] = file
-    } else if (transcodeFormats.includes(file.type)) {
-      file.type = 'mp4'
-      // file.type = 'ogg'
-      stream(req, res, file)
+    res.writeHead(206, head)
+    req.range.start = start
+    req.range.end = end
+  } else {
+    head = {
+      'Content-Length': res.file.length,
+      'Content-Type': `video/${res.file.type}`
     }
+    res.writeHead(200, head)
   }
-  console.log('end of onEngineReady')
+  // console.log(res.statusCode)
+  const opts = {start, end}
+  if (res.file.transcoded) {
+    res.send('TODO: this should be transcoded')
+  } else {
+    const stream = res.file.createReadStream(opts)
+    stream.pipe(res)
+  }
 }
 
-function keepSeeding (hash, engine) {
+
+
+// TODO use correnct param, NOT req.params.hash
+app.get('/stream/:hash',
+        exports.get,
+        exports.send)
+
+
+
+function selectFile (engine, res, next) {
+  for (let i in engine.files) {
+    let file = engine.files[i]
+    file.type = path.extname(file.name).toLowerCase().substr(1)
+    if (nativeFormats.includes(file.type)) file.transcode = false
+    else if (transcodeFormats.includes(file.type)) file.transcode = true
+    else continue
+    // We don't want to stream a trailer instead of the movie
+    if (!res.file || file.length > res.file.length) res.file = file
+  }
+  res.file.select()
+  torrentsDownloading[engine.infoHash] = res.file
+  res.file.engine = engine
+  if (res.file.transcode) {
+    res.file = transcode(res.file)
+    res.file.type = 'mp4'
+  }
+  console.log(res.file.name, res.file.type)
+  next()
+}
+
+function torrentFinished (infoHash) {
+  const file = torrentsDownloading[infoHash]
+  console.log(`Torrent ${infoHash} finished downloading`)
+  if (file.transcode) {
+    console.log('TODO It should be added to the db since it is a native format')
+  }
+  // Keep seeding the torrent to keep the swarm healthy
   const day = 24 * 60 * 60 * 1000
-  console.log(`Torrent ${hash} finished downloading`)
   setTimeout(() => {
-    delete torrentsDownloading[hash]
-    engine.destroy()
+    file.engine.destroy()
+    delete torrentsDownloading[infoHash]
   },
   15 * day) // NOTE Over 23 days it overflow
 }
 
-function stream (req, res, file) {
-  const range = req.headers.range
-  let head = {}
-  let responseCode = null
-  let streamOpts = {}
-
-  // console.log('we will stream')
-  if (range) {
-    const parts = range.replace(/bytes=/, '').split('-')
-    const start = parseInt(parts[0], 10)
-    const end = parts[1] ? parseInt(parts[1], 10) : file.length - 1
-    head = {
-      'Content-Range': `bytes ${start}-${end}/${file.length}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': (end - start) + 1,
-      'Content-Type': `video/${file.type}`
-    }
-    responseCode = 206
-    streamOpts = {start, end}
-  } else {
-    head = {
-      'Content-Length': file.length,
-      'Content-Type': `video/${file.type}`
-    }
-    responseCode = 200
-  }
-  res.writeHead(responseCode, head)
-  const stream = file.createReadStream(streamOpts)
-  // stream.pipe(res)
-  if (file.type !== 'mkv' ) stream.pipe(res)
-  else {
-    // const transcoded = ffmpeg(stream)
-    //       .format('ogg')
-    //       .videoCodec('libtheora')
-    //       .audioCodec('libvorbis')
-    //       .on('end', () => {
-    //         console.log('file has been converted succesfully')
-    //       })
-    //       .on('error', (err, stdout, stderr) => {
-    //         console.log('an error happened: ' + err.message, stdout, stderr)
-    //       })
-    //       .pipe(res, { end: true })
-    // transcoded.type = 'ogg'
-    const transcoded = ffmpeg(stream)
-          .format('mp4')
-          .videoCodec('libx264')
-          .audioCodec('libmp3lame')
-          .outputOptions('-movflags faststart')
-          .on('progress', (progress) => {
-            console.log('Processing: ' + progress)
-          })
-          .on('end', () => {
-            console.log('file has been converted succesfully')
-          })
-          .on('error', (err, stdout, stderr) => {
-            console.log('an error happened: ' + err.message, stdout, stderr)
-          })
-          .pipe(res, { end: true })
-    transcoded.type = 'mp4'
-  }
+function transcode (stream) {
+  return ffmpeg(stream)
+    .format('mp4')
+    .videoCodec('libx264')
+    .audioCodec('libmp3lame')
+    .outputOptions('-movflags faststart')
+    // .save() // TODO Save transcode
+    .on('progress', (progress) => {
+      console.log('Processing: ' + progress)
+    })
+    .on('end', () => {
+      console.log('file has been converted succesfully')
+    })
+    .on('error', (err, stdout, stderr) => {
+      console.log('an error happened: ' + err.message, stdout, stderr)
+    })
 }
