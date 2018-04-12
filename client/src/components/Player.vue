42806e0eea6c59a4897ceda8a93a2f534f09b4ab
Brice Waegeneire <brice.wge@gmail.com>
2018-04-12 23:53:59 +0200

    client: add video player

--- client/src/components/Player.vue
+++ client/src/components/Player.vue
@@ -1,25 +1,40 @@
 <template>
-  <div>
-    <h1>{{ movie.title }}</h1>
-    <div class="player-cntnr">
-      <div class="video-cntnr">
-        <iframe src="https://www.youtube.com/embed/yH4J2N3cUrw?rel=0;showinfo=0" frameborder="0" allowfullscreen=""></iframe>
-      </div>
-      <div class="comment-cntnr">
-        <input type="text" name="comment" :placeholder="$t('your-comment')">
-        <div v-for='comment in comments' v-bind:key='comment' class="comment">
-          <p>{{ comment.user_name }} - {{ $t('il-y-a') }} {{ comment.created_at }} {{ $t('ago') }}</p>
-          <p>{{ comment.content }}</p>
-        </div>
+<div>
+  <h1>{{ movie.title }}</h1>
+  <div class="player-cntnr">
+    <div class="video-cntnr">
+      <video-player class="vjs-custom-skin"
+                    :options="playerOptions"
+                    @ready="playerReadied">
+      </video-player>
+    </div>
+    <div class="comment-cntnr">
+      <input type="text" name="comment" :placeholder="$t('your-comment')">
+      <div v-for='comment in comments' v-bind:key='comment' class="comment">
+        <p>{{ comment.user_name }} - {{ $t('il-y-a') }} {{ comment.created_at }} {{ $t('ago') }}</p>
+        <p>{{ comment.content }}</p>
       </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
 // GET the path to movie file and subtitles files display it with a video tag
 // also GET all comments linked to this movie
+import 'video.js/dist/video-js.css'
+import 'vue-video-player/src/custom-theme.css'
+import { videoPlayer } from 'vue-video-player'
+import videojs from 'video.js'
+window.videojs = videojs
+// hls plugin for videojs6
+require('videojs-contrib-hls/dist/videojs-contrib-hls.js')
+
 export default {
+  components: {
+    videoPlayer
+  },
+
   data () {
     return {
       movie: {
@@ -37,7 +52,32 @@ export default {
           content: 'Josiane, les femmes à la vaisselle !!!',
           created_at: '3M d\'années'
         }
-      ]
+      ],
+      playerOptions: {
+        // videojs and plugin options
+        height: '360',
+        language: 'en',
+        sources: [ {
+          withCredentials: false,
+          type: 'application/x-mpegURL',
+          src: 'https://logos-channel.scaleengine.net/logos-channel/live/biblescreen-ad-free/playlist.m3u8'
+          // src: 'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8'
+        } ],
+        controlBar: {
+          timeDivider: false,
+          durationDisplay: false
+        },
+        poster: 'https://surmon-china.github.io/vue-quill-editor/static/images/surmon-5.jpg'
+      }
+    }
+  },
+  methods: {
+    playerReadied (player) {
+      var hls = player.tech({ IWillNotUseThisInPlugins: true }).hls
+      player.tech_.hls.xhr.beforeRequest = function (options) {
+        // console.log(options)
+        return options
+      }
     }
   }
 }
@@ -46,22 +86,24 @@ export default {
 <style lang='scss' scoped>
 @import '../assets/css/application';
 
+/* TODO use it */
 .player-cntnr{
-  width: 60%;
-  margin: 2em auto;
-  background: $dark;
-  padding: 2em;
+    width: 60%;
+    margin: 2em auto;
+    background: $dark;
+    padding: 2em;
 }
 
 h1{
-  color: $grey-txt;
-  font-size: 1.5vw;
-  font-weight: 500;
-  margin: 1em;
+    color: $grey-txt;
+    font-size: 1.5vw;
+    font-weight: 500;
+    margin: 1em;
 }
 
+/* TODO use it */
 .video-cntnr{
-  margin-bottom: 2em;
+    margin-bottom: 2em;
   position: relative;
   padding-bottom: 56.25%; /* 16:9 */
   padding-top: 25px;
