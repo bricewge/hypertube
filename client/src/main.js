6577bc74caeae92d0a5c11480864a31cbfb0ff5d
Brice Waegeneire <brice.wge@gmail.com>
2018-04-17 11:18:31 +0200

    client: auto login when registered

--- client/src/main.js
+++ client/src/main.js
@@ -25,7 +25,7 @@ Vue.use(require('@websanova/vue-auth'), {
   http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
   router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
   refreshData: {enabled: false},
-  registerData: {autoLogin: true},
+  registerData: {autoLogin: true, redirect: '/'},
   logoutData: {redirect: '/login'},
   notFoundRedirect: {path: '/'}
 })
